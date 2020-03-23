0b95794812b0bf242791a307620c10281e3e364f
daconom <62399359+daconom@users.noreply.github.com>
2020-03-23 16:01:45 +0100

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -7,15 +7,9 @@
 
 
     aws configure list --profile test
-    aws ec2 reboot-instances --region eu-central-1 --instance-ids i-008743f721dd099ff --profile test
-    aws ec2 reboot-instances --region eu-central-1 --instance-ids i-014d1b49a2a289428 --profile test
-    aws ec2 reboot-instances --region eu-central-1 --instance-ids i-0186542a71bceffac --profile test
-    aws ec2 reboot-instances --region eu-central-1 --instance-ids i-066b0ccaa5825df5a --profile test
-    aws ec2 reboot-instances --region eu-central-1 --instance-ids i-072ddf5990493157a --profile test
-    aws ec2 reboot-instances --region eu-central-1 --instance-ids i-0763a678218d19b15 --profile test
-    aws ec2 reboot-instances --region eu-central-1 --instance-ids i-0763a678218d19b15 --profile test
+    aws ec2 reboot-instances --region eu-central-1 --instance-ids i-***************** --profile test
 
-    echo "initiated reboot"
+echo "initiated reboot"
     sleep 5m
     echo "started reconnecting"
     "/c/Program Files/Git/tmp/reconnect.bat"&
